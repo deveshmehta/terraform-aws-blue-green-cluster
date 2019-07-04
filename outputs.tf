b7697d9f143c564c4c5c2e19a6082e3fd9e86148
Stephen Connor <stephen.m.connor@accenture.com>
2019-07-04 16:09:25 +0100

    Add iam policy role name export

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,7 @@ output "cluster_security_group_id" {
 output "instance_iam_role_arn" {
   value = "${aws_iam_role.instance.arn}"
 }
+
+output "instance_iam_role_name" {
+  value = "${aws_iam_role.instance.name}"
+}
