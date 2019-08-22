16da139f4919c31ac472dd884252d3e1ec0a9d9c
Mayank Kumar <mayank.k.yadav@accenture.com>
2019-08-22 14:16:16 +0100

    Testing for clb support

--- modules/cluster-asg/main.tf
+++ modules/cluster-asg/main.tf
@@ -2,40 +2,33 @@
 # Options ASG Resource Primary
 ##################################################################################
 module "cluster_asg" {
-  #source = "git::https://gitlab.awscmg-dev.dwpcloud.uk/cmg-next-generation-services/DevOps/cmg-terraform/modules/cmg-terraform-aws-autoscaling.git?ref=feature/clb_support"
-  source = "git::https://gitlab.awscmg-dev.dwpcloud.uk/cmg-next-generation-services/DevOps/cmg-terraform/modules/cmg-terraform-aws-autoscaling.git"
+  source = "git::https://gitlab.awscmg-dev.dwpcloud.uk/cmg-next-generation-services/DevOps/cmg-terraform/modules/cmg-terraform-aws-autoscaling.git?ref=feature/clb_support"
 
-  name = "${var.cluster_name}-${var.color}-svc"
+  #source = "git::https://gitlab.awscmg-dev.dwpcloud.uk/cmg-next-generation-services/DevOps/cmg-terraform/modules/cmg-terraform-aws-autoscaling.git"
 
-  create_asg_with_initial_lifecycle_hook = true
-
+  name                                        = "${var.cluster_name}-${var.color}-svc"
+  create_asg_with_initial_lifecycle_hook      = true
   initial_lifecycle_hook_name                 = "${var.cluster_name}-${var.color}-asg-lifecycle-hook"
   initial_lifecycle_hook_lifecycle_transition = "autoscaling:EC2_INSTANCE_TERMINATING"
   initial_lifecycle_hook_default_result       = "CONTINUE"
-
   # This could be a rendered data resource
   initial_lifecycle_hook_notification_metadata = <<EOF
 {
 
 }
 EOF
-
   # Launch configuration
-  lc_name = "${var.cluster_name}-${var.color}-lc"
-
-  image_id      = "${var.image_id}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${var.ssh_key_name}"
-  user_data     = "${var.user_data}"
-
+  lc_name                      = "${var.cluster_name}-${var.color}-lc"
+  image_id                     = "${var.image_id}"
+  instance_type                = "${var.instance_type}"
+  key_name                     = "${var.ssh_key_name}"
+  user_data                    = "${var.user_data}"
   security_groups              = ["${var.security_groups}"]
   target_group_arns            = ["${compact(flatten(var.target_group_arns))}"]      # in case any blanks...
   load_balancer_arns           = ["${compact(flatten(var.clb_load_balancer_arns))}"] # in case any blanks...
   iam_instance_profile         = "${var.iam_instance_profile}"
   recreate_asg_when_lc_changes = true
-
-  load_balancers = ["${var.load_balancers}"]
-
+  load_balancers               = ["${var.load_balancers}"]
   # Auto scaling group
   asg_name                  = "${var.cluster_name}-${var.color}-asg"
   vpc_zone_identifier       = "${var.subnet_ids}"
@@ -44,7 +37,6 @@ EOF
   max_size                  = "${var.max_size}"
   desired_capacity          = "${var.desired_capacity}"
   wait_for_capacity_timeout = "${var.wait_for_capacity_timeout}"
-
   tags_as_map = {
     Environment      = "${terraform.workspace}"
     Workspace        = "${terraform.workspace}"
