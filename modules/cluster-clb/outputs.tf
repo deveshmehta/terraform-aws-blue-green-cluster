cb3fcd50901333d92095e48a8c3924c5912a7ed6
Mayank Kumar <mayank.k.yadav@accenture.com>
2019-08-22 12:04:08 +0100

    Testing for clb support

--- modules/cluster-clb/outputs.tf
+++ modules/cluster-clb/outputs.tf
@@ -10,8 +10,8 @@ output "load_balancer_arn_suffix" {
   value = "${module.cluster_clb.clb_load_balancer_arn_suffix}"
 }
 
-output "load_balancer_arns" {
-  value = "${module.cluster_clb.load_balancer_arns}"
+output "clb_load_balancer_arns" {
+  value = "${module.cluster_clb.clb_load_balancer_arns}"
 }
 
 output "load_balancer_id" {
